065233dcd5ef2ab4ac03016ebbd1003fe37d80cc
sean-baek <songseon0110@gmail.com>
2021-01-10 00:15:53 +0900

    upload install_setup_packages.sh

---
+++ install_setup_packages.sh
@@ -0,0 +1,19 @@
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install net-tools -y
+sudo apt install vim -y
+sudo apt install gcc -y
+sudo apt install gcc-multilib -y
+sudo apt install g++-multilib -y
+sudo apt install binutils -y
+sudo apt install bison -y
+sudo apt install flex -y
+sudo apt install libc6-dev -y
+sudo apt install libtool -y
+sudo apt install make -y
+sudo apt install patchutils -y
+sudo apt install libgmp-dev -y
+sudo apt install libmpfr-dev -y
+sudo apt install libmpc-dev -y
+sudo apt install nasm -y
+sudo apt install qemu-kvm -y
